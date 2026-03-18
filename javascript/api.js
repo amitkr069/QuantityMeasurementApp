840d68dfc2ecf963d6804f5f4777080a18ab530c
amitkr069 <aman3133@7-11.com>
2026-03-18 14:04:26 +0530

    added a function in api.js to save history

--- javascript/api.js
+++ javascript/api.js
@@ -37,3 +37,20 @@ export async function getConversion(from, to) {
         throw error;
     }
 }
+
+export async function saveHistory(record) {
+    try {
+        const res = await fetch(`${BASE_URL}/history`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(record)
+        });
+
+        return await res.json();
+
+    } catch (error) {
+        console.error("Error saving history:", error);
+    }
+}
