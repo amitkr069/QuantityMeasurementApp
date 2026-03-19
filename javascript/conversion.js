24837eff5164cd147acaab5d25bccf76137e4d23
amitkr069 <aman3133@7-11.com>
2026-03-19 19:18:40 +0530

    Implemented the function to compare values in conversion.js file

--- javascript/conversion.js
+++ javascript/conversion.js
@@ -1,8 +1,7 @@
 /**
  * @author Amit
- * @version 7.0
- *  
- * this conversion.js file is added for use csae 7
+ * @version 8.0
+ * the compare values function is added for use csae 8
  */
 export function applyConversion(value, convObj) {
     if (!Number.isFinite(value)) {
@@ -27,4 +26,20 @@ export function applyConversion(value, convObj) {
     }
 
     throw new Error("Invalid conversion data");
+}
+
+export function compareValues(v1, u1, v2, u2, base1, base2) {
+    if (!Number.isFinite(v1) || !Number.isFinite(v2)) {
+        return "Invalid values — cannot compare";
+    }
+
+    if (base1 > base2) {
+        return `${v1} ${u1} is GREATER than ${v2} ${u2}`;
+    }
+
+    if (base1 < base2) {
+        return `${v1} ${u1} is LESS than ${v2} ${u2}`;
+    }
+
+    return `${v1} ${u1} is EQUAL to ${v2} ${u2}`;
 }
